54c50fcb8ad4e743213a654d611700d84772928e
Sohaila-Ibrahim-2001 <sohailaibrahim.jobs@gmail.com>
2026-07-21 18:06:53 +0300

    Add cruds project

---
+++ cycle48/Sessions/javascript/js/main.js
@@ -0,0 +1,89 @@
+// var age = +window.prompt("Age?");
+// switch (age > 18) {
+//   case true:
+//     console.log("Hello Adult !!!❤️❤️❤️");
+//     break;
+//   default:
+//     console.log("Hello teenAger!!!!😍😍😍");
+// }
+
+// check if number positive or negative or 0 by using switchww
+// var number = +window.prompt("Number?");
+// switch (true) {
+//   case number > 0:
+//     console.log("positive");
+//     break;
+//   case number < 0:
+//     console.log("Negative");
+//     break;
+//   default:
+//     console.log("0 is not positive or Negative");
+// }
+
+// var celender = document.getElementById("celender");
+// for (i = 1999; i <= 2026; i++) {
+//   celender.innerHTML += `<option value=" "id="celender">${i}</option>`;
+// }
+
+// sum two numbers
+// var num1 = 10;
+// var num2 = 20;
+// var sum = num1 + num2;
+// console.log(sum);
+
+// sum two numbers by using function
+// function getSum(){
+// var num1 = window.prompt("num1?");
+// var num2 = window.prompt("num2?");
+// var sum = num1 + num2;
+// console.log(sum);
+// }
+// getSum();
+
+// sum two numbers by using function (clean code ===> single responsibility )
+// var num1;
+// var num2;
+
+// function getNumbers() {
+//   num1 = +window.prompt("num1?");
+//   num2 = +window.prompt("num2?");
+// }
+
+// function getSum(x, y) {
+//   var sum = x + y;
+//   console.log(sum);
+// }
+// getNumbers();
+// getSum(num1, num2);
+
+// return statement
+// var user = "admin";
+
+// function printHello() {
+//   if (user !== "admin") {
+//     return;
+//   }
+//   console.log("hello world!❤️");
+//   console.log("hello world!❤️");
+//   console.log("hello world!❤️");
+//   var x=50;
+//   return x;
+// }
+// var x= printHello();
+// console.log(x);
+
+// scope of object
+
+var user01 = {
+  name: "Sohaila",
+  age: 25,
+  isMarried: true,
+  salary: 500,
+};
+
+var user02 = {
+  name: "Soso",
+  age: 23,
+  isMarried: true,
+  salary: 50000,
+};
---
+++ cycle48/Sessions/javascript/session4/js/main.js
@@ -0,0 +1,32 @@
+// Array >>>>>>>>>>>> object
+// var userAges = [1, 2, 3, 4];
+// console.log(userAges[1]);
+// userAges[10]="Sohaila Ibrahim";
+// for (var i = 0; i < userAges.length; i++) {
+//     console.log(userAges[i]);
+// }
+
+//  print reverse array by using for loop
+
+// that's wrong
+// var userNum = [10, 20, 30, 40, 50];
+// var newArray = [];
+// for (var i = 4; i <= 0; i--) {
+//     console.log(userNum[i]);
+//     break;
+// }
+
+// that's  right
+// var userNum = [10, 20, 30, 40, 50];
+// var newArray = [];
+// for (var i = userNum.length - 1; i >= 0; i--) {
+//     newArray[userNum.length - i - 1] = userNum[i];
+//     console.log(userNum);
+//     console.log(newArray);
+// }
+
+//  print reverse array by using reverse()
+// var numArr = [10, 20, 30, 40, 50];
+// calling ,invoke
+// numArr.reverse(numArr);
+// console.log(numArr);
